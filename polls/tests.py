e428b3ae49e8ca31f34ea95dfa27cc276f2c8ba0
Lori McCurry <lori.mccurry@gmail.com>
2015-07-15 07:38:27 -0500

    Part 5: finishing up test section and fixing misplaced methods

--- polls/tests.py
+++ polls/tests.py
@@ -6,6 +6,17 @@ from django.core.urlresolvers import reverse
 
 from .models import Question
 
+def create_question(question_text, days):
+    """
+    Creates a question with the given `question_text` published the
+    given number of `days` offset to now (negative for questions
+    published in the past, positive for questions that have yet
+    to be published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text,
+                                    pub_date=time)
+
 class QuestionMethodTests(TestCase):
 
     def test_was_published_recently_with_future_question(self):
@@ -35,17 +46,6 @@ class QuestionMethodTests(TestCase):
         recent_question = Question(pub_date=time)
         self.assertEqual(recent_question.was_published_recently(), True)
 
-    def create_question(question_text, days):
-        """
-        Creates a question with the given `question_text` published the
-        given number of `days` offset to now (negative for questions
-        published in the past, positive for questions that have yet
-        to be published).
-        """
-        time = timezone.now() + datetime.timedelta(days=days)
-        return Question.objects.create(question_text=question_text,
-                                        pub_date=time)
-
 class QuestionViewsTexts(TestCase):
     def test_index_view_with_no_questions(self):
         """
@@ -58,7 +58,7 @@ class QuestionViewsTexts(TestCase):
 
     def test_index_view_with_a_past_question(self):
         """
-        Questions with a pub_date in teh past should be displayed on
+        Questions with a pub_date in the past should be displayed on
         the index page.
         """
         create_question(question_text="Past question.", days=-30)
@@ -103,3 +103,22 @@ class QuestionViewsTexts(TestCase):
             response.context['latest_question_list'],
             ['<Question: Past question 2.>', '<Question: Past question 1.>']
         )
+
+class QuestionIndexDetailTests(TestCase):
+    def test_detail_view_with_a_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future should
+        return a 404 not found.
+        """
+        future_question = create_question(question_text='Future question.', days=5)
+        response = self.client.get(reverse('polls:detail', args=(future_question.id,)))
+        self.assertEqual(response.status_code, 404)
+
+    def test_detail_view_with_a_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past should
+        return a 404 not found.
+        """
+        past_question = create_question(question_text='Past question.', days=-5)
+        response = self.client.get(reverse('polls:detail', args=(past_question.id,)))
+        self.assertContains(response, past_question.question_text, status_code=200)
--- polls/views.py
+++ polls/views.py
@@ -11,7 +11,7 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions (not including those set to be published in the future."""
+        """Return the last five published questions (not including those set to be published in the future.)"""
         return Question.objects.filter(
            pub_date__lte=timezone.now()
         ).order_by('-pub_date')[:5]
@@ -19,6 +19,12 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
